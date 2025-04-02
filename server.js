cb852bd77f9ef9d81d3ccd5b0fd05f69b84e44b4
evanhinde <200529232@student.georgianc.on.ca>
2025-04-02 19:19:08 -0400

    React

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-// Set up a basic route
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+app.use(cors()); // Enable CORS
+app.use(express.json()); // Enable JSON parsing
 
-// Start the server
+// Serve React static files in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('Express backend is running');
+  });
+}
+
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
