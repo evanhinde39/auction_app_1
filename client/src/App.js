751bd24900be485df58c714a7cc36b38117c0a4e
evanhinde <200529232@student.georgianc.on.ca>
2025-04-02 19:17:00 -0400

    React added

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Sports Auction App</h1>
+      <p>Start bidding on teams now!</p>
+    </div>
+  );
+}
+
+export default App;
